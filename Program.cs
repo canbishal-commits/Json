3ad16fe5fd018a8efbcd8884918fb006a9633601
canbishal-commits <canbishal@gmail.com>
2026-05-23 15:45:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "users.json";
+
+        string jsonData = File.ReadAllText(filePath);
+
+        List<User> users = JsonConvert.DeserializeObject<List<User>>(jsonData);
+
+        Console.WriteLine("Users List:\n");
+
+        foreach (User user in users)
+        {
+            Console.WriteLine($"Name: {user.Name}");
+            Console.WriteLine($"Age: {user.Age}");
+            Console.WriteLine($"City: {user.City}");
+            Console.WriteLine();
+        }
+    }
+}
